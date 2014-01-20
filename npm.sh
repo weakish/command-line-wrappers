f9841c76ef4bba13b111bc92d27ecec147a4deb9
Jakukyo Friel <weakish@gmail.com>
2014-01-20 21:06:38 +0800

    npm.sh: fix recursive call bug

--- npm.sh
+++ npm.sh
@@ -2,5 +2,5 @@
 
 case $1 in 
   docs | faq | find | home | i | info | install | isntall | issues | rb | rebuild | repo | s | se | search | v | view) cnpm "$@" ;;
-  *) npm "$@" ;;
+  *) /usr/bin/npm "$@" ;;
 esac
