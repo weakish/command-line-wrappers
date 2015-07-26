5872234e357b8583db44aa4005d8205c100aa257
Jakukyo Friel <weakish@gmail.com>
2015-07-26 11:49:08 +0800

    Import cat-videos from gist/7433160.
    cat-videos.sh is a sh script to combine multiple video files into
    one.

---
+++ cat-videos.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# by Jakukyo Friel <weakish@gmail.com>, under Apache License v2.
+
+# Combine videos into one.
+# Note: you need a recent version of ffmpeg, see http://ffmpeg.org/download.htm
+# Usage:
+#     cd /path/to/videos && cat-videos output_file_name
+# The directory should only contains video files you want to combine, and in the proper order.
+
+for f in *; do
+  echo "file '$f'" >> VIDEO_LIST;
+done
+ffmpeg -f concat -i VIDEO_LIST -c copy $1
